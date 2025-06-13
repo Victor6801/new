2acf9582eb28be30969515c56632b58a40010c18
Victor6801 <55526652+Victor6801@users.noreply.github.com>
2025-06-13 11:28:56 +0800

    Create sample.ts

---
+++ sample.ts
@@ -0,0 +1,19 @@
+// Sample file for testing scripts.
+let _id;
+let fb_id: any;
+fb_id = true;
+fb_id = 23423523;
+
+const firstName = "Riyad";
+const lastName: string = "Hossain";
+let isLoading: boolean;
+isLoading = false;
+
+let roll: string | number = 423;
+roll = "423";
+
+let device: string | number | boolean; // Union Type
+device = "Mobile";
+device = 4323523;
+
+const re: RegExp = /\w+/g;
